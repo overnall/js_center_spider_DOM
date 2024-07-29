62026f834e2fb51200ce278ddb16adc582fa8208
Olexander <v.h.olexander@gmail.com>
2024-07-29 12:40:37 +0300

    make build

---
+++ index.aa23ee2d.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),l=t.clientWidth/2,c=t.clientHeight/2,i=e.offsetWidth/2;e.style.left=l-i+"px",e.style.top=c-i+"px";
+//# sourceMappingURL=index.aa23ee2d.js.map
